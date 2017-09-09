abde5e5993af0ce1799b2fbf95d44e3c49b31cba
MTorosyan <massistorosyan@gmail.com>
2017-09-09 23:03:27 +0100

    slider bug fix
    +fixed bug where invalid/incomplete code would prevent sliders from
    functioning

--- animation.js
+++ animation.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  console.log("ready!");
 
-    $('#resetbutton').click(function() {
+  $('#resetbutton').click(function() {
 
     $('#breathins').val('5');
     $('#breathint').val('5');
@@ -15,50 +15,50 @@ $( document ).ready(function() {
     $('#breathoutholds').val('5');
     $('#breathoutholdt').val('5');
 
-    });
+  });
 
-    //breath in settings
-    $('#breathins').on('change', function(){
-        $('#breathint').val($('#breathins').val());
-    });
+  //breath in settings
+  $('#breathins').on('change', function() {
+    $('#breathint').val($('#breathins').val());
+  });
 
-    $('#breathint').on('keyup', function(){
-        $('#breathins').val($('#breathint').val());
-    });
+  $('#breathint').on('keyup', function() {
+    $('#breathins').val($('#breathint').val());
+  });
 
-    //breath in hold settings
-    $('#breathholdins').on('change', function(){
-        $('#breathholdint').val($('#breathholdins').val());
-    });
+  //breath in hold settings
+  $('#breathholdins').on('change', function() {
+    $('#breathholdint').val($('#breathholdins').val());
+  });
 
-    $('#breathholdint').on('keyup', function(){
-        $('#breathholdins').val($('#breathholdint').val());
-    });
+  $('#breathholdint').on('keyup', function() {
+    $('#breathholdins').val($('#breathholdint').val());
+  });
 
-    //breath out settings
-    $('#breathouts').on('change', function(){
-        $('#breathoutt').val($('#breathouts').val());
-    });
+  //breath out settings
+  $('#breathouts').on('change', function() {
+    $('#breathoutt').val($('#breathouts').val());
+  });
 
-    $('#breathoutt').on('keyup', function(){
-        $('#breathouts').val($('#breathoutt').val());
-    });
+  $('#breathoutt').on('keyup', function() {
+    $('#breathouts').val($('#breathoutt').val());
+  });
 
-    //breath out hold settings
-    $('#breathoutholds').on('change', function(){
-        $('#breathoutholdt').val($('#breathoutholds').val());
-    });
+  //breath out hold settings
+  $('#breathoutholds').on('change', function() {
+    $('#breathoutholdt').val($('#breathoutholds').val());
+  });
 
-    $('#breathoutholdt').on('keyup', function(){
-        $('#breathoutholds').val($('#breathoutholdt').val());
-    });
+  $('#breathoutholdt').on('keyup', function() {
+    $('#breathoutholds').val($('#breathoutholdt').val());
+  });
 
-    //animation control
-     timeout = setTimeout(function(){
-
-       , 500);
-    // $('.circle').on('click', function(){
-    //   $('.circle').addClass('shrink');
-    //     });
+  //animation control
+  //  timeout = setTimeout(function(){
+  //
+  //    , 500);
+  // $('.circle').on('click', function(){
+  //   $('.circle').addClass('shrink');
+  //     });
 
 });
